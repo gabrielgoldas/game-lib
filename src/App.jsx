fa1d0ae0cb475ceb3c4151d96288bc278be72ef3
Gabriel Goldas <gabriel.goldas@gmail.com>
2025-02-07 13:58:28 -0300

    refactoring the App.jsx by creating an useGameCollection, responsible pfor adding, removing and having the games's list

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,9 @@
-import { useState } from "react"
 import Game from "./components/Game"
 import NewGameForm from "./components/NewGameForm"
+import useGameCollection from "./hooks/useGameCollection"
 
 export default function App() {
-  const [games, setGames] = useState(() => {
-    const storedGames = localStorage.getItem("obc-game-lib")
-    if (!storedGames) return []
-    return JSON.parse(storedGames)
-  })
-  
-  const addGame = ({ title, cover }) => {
-    const id = Math.floor(Math.random() * 100000000)
-    const game = { id, title, cover }
-    setGames(state => {
-      const newState = [...state, game]
-      localStorage.setItem("obc-game-lib", JSON.stringify(newState))
-      return newState
-    })
-  }
-
-  const removeGame = (id) => {
-    setGames(state => {
-      const newState = state.filter(game => game.id !== id)
-      localStorage.setItem("obc-game-lib", JSON.stringify(newState))
-      return newState
-    })
-  }
-
+  const { games, addGame, removeGame } = useGameCollection()
   return (
     <div id="app">
       <h1>Biblioteca de Jogos</h1>
